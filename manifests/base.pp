c1048ad9ce3ee75a7decd785348ace0b165371b0
Alexandr Kulitsky <to.kulitsky@outlook.com>
2013-09-30 15:57:56 +0300

    updated iptables config

--- manifests/base.pp
+++ manifests/base.pp
@@ -20,6 +20,32 @@ package { $misc_packages: ensure => latest }
 class { "ntp": autoupdate => true }
 class { 'htop': }
 
+# Iptables (Firewall) package and rules to allow ssh, http, https and dns services.
+class iptables {
+	package { "iptables":
+		ensure => present
+	}
+
+	service { "iptables":
+		require => Package["iptables"],
+		hasstatus => true,
+		status => "true",
+		hasrestart => false,
+	}
+
+	file { "/etc/sysconfig/iptables":
+		owner   => "root",
+		group   => "root",
+		mode    => 600,
+		replace => true,
+		ensure  => present,
+		source  => "/vagrant/files/iptables.txt",
+		require => Package["iptables"],
+		notify  => Service["iptables"],
+	}
+}
+class { 'iptables': }
+
 class { 'apache':
 	sendfile		=> 'off'
 }
